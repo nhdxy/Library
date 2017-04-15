13773739962b26f5b5c53b0e77efc09c6a06cb2c
nhdxy <731566569@qq.com>
2017-04-15 22:51:53 +0800

    Initial commit

--- mylibrary/src/main/java/com/andrnhd/utils/SkitUtils.kt
+++ mylibrary/src/main/java/com/andrnhd/utils/SkitUtils.kt
@@ -10,31 +10,37 @@ import android.support.v4.app.Fragment
  * Created by nhd on 2017/4/15.
  */
 fun <T> Context.skit(clazz: Class<T>, bundle: Bundle = Bundle(), isFinished: Boolean = false) {
-    if (this is Activity) {
-        val intent = Intent(this, clazz)
-        intent.putExtras(bundle)
-        this.startActivity(intent)
-        if (isFinished) {
-            this.finish()
+    when (this) {
+        is Activity -> {
+            val intent = Intent(this, clazz)
+            intent.putExtras(bundle)
+            this.startActivity(intent)
+            if (isFinished) {
+                this.finish()
+            }
         }
-    } else if (this is Fragment) {
-        val intent = Intent(this, clazz)
-        intent.putExtras(bundle)
-        this.activity.startActivity(intent)
-        if (isFinished) {
-            this.activity.finish()
+        is Fragment -> {
+            val intent = Intent(this, clazz)
+            intent.putExtras(bundle)
+            this.activity.startActivity(intent)
+            if (isFinished) {
+                this.activity.finish()
+            }
         }
     }
 }
 
 fun <T> Context.skitForResult(clazz: Class<T>, requestCode: Int, bundle: Bundle = Bundle()) {
-    if (this is Activity) {
-        val intent = Intent(this, clazz)
-        intent.putExtras(bundle)
-        this.startActivityForResult(intent, requestCode)
-    } else if (this is Fragment) {
-        val intent = Intent(this, clazz)
-        intent.putExtras(bundle)
-        this.startActivityForResult(intent, requestCode)
+    when (this) {
+        is Activity -> {
+            val intent = Intent(this, clazz)
+            intent.putExtras(bundle)
+            this.startActivityForResult(intent, requestCode)
+        }
+        is Fragment -> {
+            val intent = Intent(this, clazz)
+            intent.putExtras(bundle)
+            this.startActivityForResult(intent, requestCode)
+        }
     }
 }
